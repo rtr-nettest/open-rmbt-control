dea3fc5a940b8a8e49a6c3351f2445971bef0bf4
Yurii Chukhlatyi <ychukhlatyi@evenz.co>
2021-02-04 19:40:51 +0200

    Fix cors

--- src/main/java/at/rtr/rmbt/config/WebMvcConfiguration.java
+++ src/main/java/at/rtr/rmbt/config/WebMvcConfiguration.java
@@ -20,7 +20,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+//        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Configuration
