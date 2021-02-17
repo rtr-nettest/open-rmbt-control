f17037541cb32efe81093ccbf1fc8511d3965df3
Oleksii Semenov <sendtosemenov@gmail.com>
2021-02-17 04:58:12 +0000

    ONT-2205 # Signal Permissions

--- src/main/java/at/rtr/rmbt/config/WebMvcConfiguration.java
+++ src/main/java/at/rtr/rmbt/config/WebMvcConfiguration.java
@@ -1,5 +1,7 @@
 package at.rtr.rmbt.config;
 
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static at.rtr.rmbt.constant.URIConstants.*;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -20,22 +24,32 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Configuration
     public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+        @Value("${auth0.apiAudience}")
+        private String audience;
+        @Value("${auth0.issuer}")
+        private String issuer;
 
         @Override
         protected void configure(HttpSecurity httpSecurity) throws Exception {
             httpSecurity.cors();
+            JwtWebSecurityConfigurer.forRS256(audience, issuer)
+                    .configure(httpSecurity)
+                    .authorizeRequests()
+                    .antMatchers(SIGNAL_REQUEST, SIGNAL_RESULT, NEWS_URL, REGISTRATION_URL,
+                            RESULT_QOS_URL, RESULT_URL, SETTINGS_URL).permitAll()
+                    .antMatchers(ADMIN_SIGNAL).hasAuthority("read:reports/signal")
+                    .anyRequest().authenticated();
         }
 
         @Override
         public void configure(WebSecurity web) throws Exception {
             // ignoring security for swagger APIs
-            web.ignoring().antMatchers("/**", "/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**", "/health").and()
+            web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**", "/health").and()
                     .ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
         }
     }
