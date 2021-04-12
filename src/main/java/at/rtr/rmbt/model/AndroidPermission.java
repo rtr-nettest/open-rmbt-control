ed3e625397e957f9f882071908743d052df3453d
Yurii Chukhlatyi <ychukhlatyi@evenz.co>
2021-04-12 07:11:12 +0300

    ONT-2643 Fix hibernate entity tracking

--- src/main/java/at/rtr/rmbt/model/AndroidPermission.java
+++ src/main/java/at/rtr/rmbt/model/AndroidPermission.java
@@ -2,15 +2,13 @@ package at.rtr.rmbt.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class AndroidPermission {
 
     @ApiModelProperty(notes = "Whole name of the android permission", example = "android.permission.ACCESS_FINE_LOCATION")
