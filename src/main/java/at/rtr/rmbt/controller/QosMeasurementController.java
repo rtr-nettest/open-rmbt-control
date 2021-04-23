deae28520f2049adf6551911ff8c740093e2dbeb
oleksis <oleksis@jfrog.com>
2021-04-23 13:32:55 +0300

    Add get to test

--- src/main/java/at/rtr/rmbt/controller/QosMeasurementController.java
+++ src/main/java/at/rtr/rmbt/controller/QosMeasurementController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -42,6 +43,12 @@ public class QosMeasurementController {
     @ApiOperation("Save QoS test results")
     @PostMapping(RESULT_QOS_URL)
     public ErrorResponse saveQosMeasurementResult(@RequestBody QosResultRequest qosResultRequest) {
+        return qosMeasurementService.saveQosMeasurementResult(qosResultRequest);
+    }
+
+    @ApiOperation("Save QoS test results")
+    @GetMapping(RESULT_QOS_URL)
+    public ErrorResponse saveQosMeasurementResultGet(@RequestBody QosResultRequest qosResultRequest) {
         return qosMeasurementService.saveQosMeasurementResult(qosResultRequest);
     }
 }
