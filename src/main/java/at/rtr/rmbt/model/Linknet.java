4a8374646e3d0401380501a15d51c1d1273a46a4
oleksis <oleksis@jfrog.com>
2021-05-25 10:34:20 +0300

    Fix QoS

--- src/main/java/at/rtr/rmbt/model/Linknet.java
+++ src/main/java/at/rtr/rmbt/model/Linknet.java
@@ -3,6 +3,7 @@ package at.rtr.rmbt.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "linknet")
-public class Linknet {
+public class Linknet implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
